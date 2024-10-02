d0c42ec3d0409f001ada683aed27560d39a9f9de
ssk2 <1sqlroot@gmail.com>
2024-10-02 10:52:12 +0400

    backup and add /get

---
+++ internal/app/commands/get.go
@@ -0,0 +1,10 @@
+package commands
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func (c *Commander) Get(inputMessage *tgbotapi.Message) {
+	msg := tgbotapi.NewMessage(inputMessage.Chat.ID, "ABC")
+	c.bot.Send(msg)
+}
